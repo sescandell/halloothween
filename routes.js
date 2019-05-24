8192d0a58f959d9ce7569ee2f39412697064792b
Stéphane Escandell <stephane.escandell@gmail.com>
2019-05-24 22:52:41 +0200

    Ajout manifest

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ module.exports = function(app,io){
     var camera = undefined;
     // Storage in-memory des photos précédentes
     var picturesStore = new InMemoryStore(100);
-    //*
+    /*
     // killall  PTPCamera
     var gphoto = new GPhoto.GPhoto2();
     gphoto.list(function(cameras){
