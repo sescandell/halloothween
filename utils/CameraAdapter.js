b08b75c2059688a33b4fa8be38d0622919196789
Sescandell <stephane.escandell@gmail.com>
2026-01-25 18:04:27 +0100

    refactor(esm): convert CameraAdapter to async factory function

--- utils/CameraAdapter.js
+++ utils/CameraAdapter.js
@@ -9,26 +9,22 @@
  * la compatibilité avec gphoto2 en production sur Raspberry Pi
  */
 
-var WebcamCamera = require('./WebcamCamera');
-var GPhotoCamera = require('./GPhotoCamera');
-
 /**
- * Factory qui retourne l'implémentation appropriée selon la plateforme
+ * Factory function async qui retourne l'implémentation appropriée selon la plateforme
+ * @returns {Promise<WebcamCamera|GPhotoCamera>}
  */
-class CameraAdapter {
-    constructor() {
-        var platform = process.platform;
-        
-        if (platform === 'win32') {
-            // Windows : Mode développement avec webcam
-            console.log('[CAMERA] Détection de Windows - Utilisation de la webcam système');
-            return new WebcamCamera();
-        } else {
-            // Linux/Unix : Mode production avec gphoto2
-            console.log('[CAMERA] Détection de ' + platform + ' - Utilisation de gphoto2');
-            return new GPhotoCamera();
-        }
+export async function createCameraAdapter() {
+    const platform = process.platform;
+    
+    if (platform === 'win32') {
+        // Windows : Mode développement avec webcam
+        console.log('[CAMERA] Détection de Windows - Utilisation de la webcam système');
+        const { WebcamCamera } = await import('./WebcamCamera.js');
+        return new WebcamCamera();
+    } else {
+        // Linux/Unix : Mode production avec gphoto2
+        console.log('[CAMERA] Détection de ' + platform + ' - Utilisation de gphoto2');
+        const { GPhotoCamera } = await import('./GPhotoCamera.js');
+        return new GPhotoCamera();
     }
 }
-
-module.exports = CameraAdapter;
